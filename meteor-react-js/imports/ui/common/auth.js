822fd74d3df059b799c1903abb68415d06ca3218
Serhii [boonya] Buinytskyi <boonya41@gmail.com>
2019-05-17 15:20:47 +0300

    Preloader on auth/noauth protectors

--- meteor-react-js/imports/ui/common/auth.js
+++ meteor-react-js/imports/ui/common/auth.js
@@ -1,22 +1,43 @@
+import { createSelector } from "reselect";
 import { connectedRouterRedirect } from "redux-auth-wrapper/history4/redirect";
 import locationHelperBuilder from "redux-auth-wrapper/history4/locationHelper";
 
 import ROUTES from "../../routes";
-import Preloader from '../common/Preloader';
-import { isReady } from '../redux/utils/valueState';
+import Preloader from "../common/Preloader";
+import { isReady, isLoading } from "../redux/utils/valueState";
 
-export const isAuth = ({ me }) => isReady(me) && !!me;
+const isAuthSelector = createSelector(
+  ({ me }) => isReady(me) && !!me,
+  isAuth => isAuth
+);
+
+const isNotAuthSelector = createSelector(
+  ({ me }) => isReady(me) && !me,
+  isNotAuth => isNotAuth
+);
+
+const isLoadingSelector = createSelector(
+  ({ me }) => isLoading(me),
+  isLoading => isLoading
+);
+
+const getBackRedirectPath = (_, ownProps) => {
+  return locationHelperBuilder({}).getRedirectQueryParam(ownProps) || ROUTES.Landing;
+};
 
 export const authOnly = connectedRouterRedirect({
   redirectPath: ROUTES.Login,
-  authenticatedSelector: state => isAuth(state),
+  authenticatedSelector: isAuthSelector,
+  authenticatingSelector: isLoadingSelector,
   AuthenticatingComponent: Preloader,
   wrapperDisplayName: "UserIsAuthenticated",
 });
 
 export const noAuthOnly = connectedRouterRedirect({
-  redirectPath: (state, ownProps) => locationHelperBuilder({}).getRedirectQueryParam(ownProps) || ROUTES.Landing,
+  redirectPath: getBackRedirectPath,
   allowRedirectBack: false,
-  authenticatedSelector: state => !isAuth(state),
+  authenticatedSelector: isNotAuthSelector,
+  authenticatingSelector: isLoadingSelector,
+  AuthenticatingComponent: Preloader,
   wrapperDisplayName: "UserIsNotAuthenticated",
 });
