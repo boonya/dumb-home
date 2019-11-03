7281ca386968d4d01b64fd5b7e57ec0369e98f22
Serhii [boonya] Buinytskyi <boonya41@gmail.com>
2019-11-03 18:41:56 +0200

    Dark theme for storybook

--- app/.storybook/config.js
+++ app/.storybook/config.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { configure, addDecorator } from "@storybook/react";
+import { configure, addParameters, addDecorator } from "@storybook/react";
+import { themes } from "@storybook/theming";
 import { withKnobs } from "@storybook/addon-knobs";
 
 import { CssBaseline } from "@material-ui/core";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 
 import theme from "../imports/ui/theme";
+
+addParameters({
+  options: {
+    theme: themes.dark,
+  },
+});
 
 addDecorator(withKnobs);
 addDecorator(fn => (
