e84832c48fc46f1f8a165298e938edc20c7c2aa1
Serhii [boonya] Buinytskyi <boonya41@gmail.com>
2019-11-17 17:41:22 +0200

    Build Stages, tests & lint

--- app/tests/main.js
+++ app/tests/main.js
@@ -1,12 +1,9 @@
+/* eslint camelcase: 0 */
+/* eslint global-require: 0 */
 import { Meteor } from 'meteor/meteor';
 import assert from 'assert';
 
 describe('app', () => {
-  it('package.json has correct name', async () => {
-    const { name } = await import('../package.json');
-    assert.strictEqual(name, 'home');
-  });
-
   if (Meteor.isClient) {
     it('client is not server', () => {
       assert.strictEqual(Meteor.isServer, false);
@@ -18,4 +15,28 @@ describe('app', () => {
       assert.strictEqual(Meteor.isClient, false);
     });
   }
+});
+
+describe('Manifest, Theme and package.json', async () => {
+  const { name, ...pkg } = await require('../package.json');
+  const { theme_color, background_color, ...manifest } = await require('../public/manifest.json');
+  const theme = require('../imports/ui/theme.js');
+
+  it('Package name is "dumb-home"', () => {
+    assert.strictEqual(name, 'dumb-home');
+  });
+
+  it('Package & manifest props are equal', () => {
+    assert.strictEqual(pkg.version, manifest.version);
+    assert.strictEqual(pkg.description, manifest.description);
+    assert.strictEqual(pkg.author, manifest.author);
+  });
+
+  it('Theme Color in manifest.json is equal to Application Theme Color', () => {
+    assert.strictEqual(theme_color, theme.default.palette.primary.main);
+  });
+
+  it('Backgrount Color in manifest.json is equal to Application Theme Background Color', () => {
+    assert.strictEqual(background_color, theme.default.palette.background.default);
+  });
 });
