339e43c63286eacf04c211c8044b0a1c8548dfb3
Serhii [boonya] Buinytskyi <boonya41@gmail.com>
2020-05-24 14:32:07 +0300

    Forgotten

--- app/imports/api/camera.js
+++ app/imports/api/camera.js
@@ -30,22 +30,6 @@ const edit = async ({ _id, label, username, password }) => (
   Devices.update({ _id }, { $set: { label, username, password } })
 );
 
-// const startRecord = (id, name, uri) => new Promise((resolve, reject) => {
-//   Meteor.call(METHODS.CAMERA_RECORD_START, { id, name, uri }, (err, response) => {
-//     if (err) return reject(err);
-//     // Devices.update({ _id: id }, { $set: { recording: true } });
-//     resolve(response);
-//   });
-// });
-
-// const stopRecord = (id) => new Promise((resolve, reject) => {
-//   Meteor.call(METHODS.CAMERA_RECORD_STOP, { id }, (err, response) => {
-//     if (err) return reject(err);
-//     // Devices.update({ _id: id }, { $set: { recording: false } });
-//     resolve(response);
-//   });
-// });
-
 const getStreamUri = (hostname, port, username, password) => new Promise((resolve, reject) => {
   Meteor.call(METHODS.CAMERA_GET_STREAM_URI, { hostname, port, username, password }, (err, uri) => {
     if (err) return reject(err);
@@ -57,7 +41,6 @@ const startRecord = async (id) => {
   const { label, details, username, password } = Devices.findOne({ _id: id });
   const { hostname, port } = details;
   const uri = await getStreamUri(hostname, port, username, password);
-  // await startRecord(id, label, uri);
   return new Promise((resolve, reject) => {
     Meteor.call(METHODS.CAMERA_RECORD_START, { id, name: label, uri }, (err, response) => {
       if (err) return reject(err);
