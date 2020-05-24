b396ae022363b676d7ee95632368336c80ade29f
Serhii [boonya] Buinytskyi <boonya41@gmail.com>
2020-05-24 14:55:49 +0300

    Recorder camera api (#28)
    * Splitted start/stop record epics, fixed on.stopped handler
    * nginx config to router

--- app/imports/server/recorder.js
+++ app/imports/server/recorder.js
@@ -1,93 +1,112 @@
-import Recorder, { RecorderEvents } from 'rtsp-video-recorder';
+import RtspRecorder, { RecorderEvents } from 'rtsp-video-recorder';
 import { RECORDER } from '../config';
 import DEVICES from '../devices';
 import Devices from '../collections/devices';
 import api from '../api/camera';
 
-const Registry = {};
+class Recorder {
+  Registry = {};
 
-const getRecorderInstance = (id, uri, name) => {
-  const stackLimit = 100;
-  const progressStack = [];
+  getRecorderInstance = (id, uri, name) => {
+    const { FOLDER, SEGMENT_TIME, DIR_SIZE_THRESHOLD, AUTO_CLEAR } = RECORDER;
 
-  const { FOLDER, SEGMENT_TIME, DIR_SIZE_THRESHOLD, AUTO_CLEAR } = RECORDER;
+    const recorder = new RtspRecorder(uri, FOLDER, {
+      title: name,
+      filenamePattern: `%H.%M.%S-${name.replace(/%/g, '%%')}`,
+      segmentTime: SEGMENT_TIME,
+      dirSizeThreshold: DIR_SIZE_THRESHOLD,
+      autoClear: AUTO_CLEAR,
+    });
 
-  const recorder = new Recorder(uri, FOLDER, {
-    title: name,
-    filenamePattern: `%H.%M.%S-${name.replace(/%/g, '%%')}`,
-    segmentTime: SEGMENT_TIME,
-    dirSizeThreshold: DIR_SIZE_THRESHOLD,
-    autoClear: AUTO_CLEAR,
-  });
+    recorder.on(RecorderEvents.STARTED, (...args) => {
+      console.log(` ${new Date()}; ${RecorderEvents.STARTED}; "${name}": `, ...args);
+    });
 
-  recorder.on(RecorderEvents.STARTED, (...args) => {
-    console.log(`Recorder started "${name}": `, ...args);
-  });
+    recorder.on(RecorderEvents.STOPPED, (...args) => {
+      console.log(` ${new Date()}; ${RecorderEvents.STOPPED}; "${name}": `, ...args);
+    });
 
-  recorder.on(RecorderEvents.STOPPED, (...args) => {
-    console.log(`Recorder stopped "${name}": `, ...args);
+    recorder.on(RecorderEvents.STOP, (...args) => {
+      console.log(` ${new Date()}; ${RecorderEvents.STOP}; "${name}": `, ...args);
+    });
 
-    console.log('\n/** *********************** */\n');
-    console.log(`These are last ${progressStack.length} progress events of camera "${name}": \n`);
-    progressStack.forEach((item) => console.log(item));
-    console.log('\n/** *********************** */\n');
-  });
+    recorder.on(RecorderEvents.PROGRESS, (buffer) => {
+      const payload = buffer.toString();
+      // frame=  638 fps= 21 q=-1.0 size=N/A time=00:00:31.85 bitrate=N/A speed=1.04x
+      // [rtsp @ 0x7f82a8800000] max delay reached.
+      // [rtsp @ 0x7f82a8800000] RTP: missed 101 packets
+      if (
+        /^\s*\[rtsp @ 0x[0-9a-f]+\] max delay reached/.test(payload)
+        || /^\s*\[rtsp @ 0x[0-9a-f]+\] RTP: missed/.test(payload)
+        || /^\s*frame=/.test(payload)
+      ) {
+        return;
+      }
+      console.log(` ${new Date()}; ${RecorderEvents.PROGRESS}; "${name}": `, payload);
+    });
 
-  recorder.on(RecorderEvents.PROGRESS, (buffer) => {
-    progressStack.push(buffer.toString());
-    if (progressStack.length > stackLimit) {
-      progressStack.splice(progressStack.length - stackLimit);
+    recorder.on(RecorderEvents.ERROR, (...args) => {
+      console.log(` ${new Date()}; ${RecorderEvents.ERROR}; "${name}": `, ...args);
+    });
+
+    recorder.on(RecorderEvents.SEGMENT_STARTED, (...args) => {
+      console.log(` ${new Date()}; ${RecorderEvents.SEGMENT_STARTED}; "${name}": `, ...args);
+    });
+
+    recorder.on(RecorderEvents.DIRECTORY_CREATED, (...args) => {
+      console.log(` ${new Date()}; ${RecorderEvents.DIRECTORY_CREATED}; "${name}": `, ...args);
+    });
+
+    recorder.on(RecorderEvents.FILE_CREATED, (...args) => {
+      console.log(` ${new Date()}; ${RecorderEvents.FILE_CREATED}; "${name}": `, ...args);
+    });
+
+    recorder.on(RecorderEvents.SPACE_FULL, (...args) => {
+      console.log(` ${new Date()}; ${RecorderEvents.SPACE_FULL}; "${name}": `, ...args);
+    });
+
+    recorder.on(RecorderEvents.SPACE_WIPED, (...args) => {
+      console.log(` ${new Date()}; ${RecorderEvents.SPACE_WIPED}; "${name}": `, ...args);
+    });
+
+    return recorder;
+  };
+
+  start = ({ id, name, uri }) => {
+    const recorder = this.getRecorderInstance(id, uri, name);
+    recorder
+      .on('stopped', Meteor.bindEnvironment(() => {
+        Devices.update({ _id: id }, { $set: { recording: false } });
+      }))
+      .start();
+    Devices.update({ _id: id }, { $set: { recording: true } });
+    this.Registry[id] = recorder;
+  };
+
+  stop = ({ id }) => {
+    const recorder = this.Registry[id];
+    if (recorder) {
+      recorder.stop();
     }
-  });
+    Devices.update({ _id: id }, { $set: { recording: false } });
+  };
 
-  recorder.on(RecorderEvents.ERROR, (...args) => {
-    console.log(`Recorder error "${name}": `, ...args);
-  });
+  startup = () => {
+    const promises = Devices.find({ type: DEVICES.CAMERA, recording: true })
+      .fetch()
+      .map(async ({ _id, label, details, username, password }) => {
+        const { hostname, port } = details;
+        const uri = await api.getStreamUri(hostname, port, username, password);
+        this.start({ id: _id, name: label, uri });
+      });
+    Promise.all(promises);
+  };
+}
 
-  recorder.on(RecorderEvents.SEGMENT_STARTED, (...args) => {
-    console.log(`Recorder segment started "${name}": `, ...args);
-  });
-
-  recorder.on(RecorderEvents.DIRECTORY_CREATED, (...args) => {
-    console.log(`Recorder directory created "${name}": `, ...args);
-  });
-
-  recorder.on(RecorderEvents.FILE_CREATED, (...args) => {
-    console.log(`Recorder file created "${name}": `, ...args);
-  });
-
-  recorder.on(RecorderEvents.SPACE_FULL, (...args) => {
-    console.log(`Recorder space full "${name}": `, ...args);
-  });
-
-  recorder.on(RecorderEvents.SPACE_WIPED, (...args) => {
-    console.log(`Recorder space wiped "${name}": `, ...args);
-  });
-
-  return recorder;
-};
-
-const start = async ({ id, name, uri }) => {
-  const recorder = getRecorderInstance(id, uri, name);
-  recorder.start();
-  Registry[id] = recorder;
-  return recorder;
-};
-
-const stop = async ({ id }) => {
-  const recorder = Registry[id];
-  if (recorder) {
-    recorder.stop();
-  }
-};
-
-const startup = () => {
-  const cameras = Devices.find({ type: DEVICES.CAMERA, recording: true }).fetch();
-  Promise.all(cameras.map(({ _id, recording }) => api.record({ _id, recording })));
-};
+const recorder = new Recorder();
 
 export default {
-  start,
-  stop,
-  startup,
+  start: recorder.start,
+  stop: recorder.stop,
+  startup: recorder.startup,
 };
